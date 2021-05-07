58baec51872e0c5ef6a17a3496c8f6554b7e85dd
aaaaaaaaaaalex <aleks.ru.mail.77@mail.ru>
2021-05-07 17:57:41 +0400

    сверстал около половины страницы

--- app/js/script_jQuery.js
+++ app/js/script_jQuery.js
@@ -11,4 +11,11 @@ jQuery(function() {
 		arrows: false,
 		adaptiveHeight: true
 	});
+	jQuery("#slider-2").slick({
+		centerMode: true,
+		centerPadding: '490px',
+		slidesToShow: 1,
+		arrows: false,
+		adaptiveHeight: true
+	});
 });
--- yogatur/js/script_jQuery.js
+++ yogatur/js/script_jQuery.js
@@ -3446,4 +3446,11 @@ jQuery(function() {
 		arrows: false,
 		adaptiveHeight: true
 	});
+	jQuery("#slider-2").slick({
+		centerMode: true,
+		centerPadding: '490px',
+		slidesToShow: 1,
+		arrows: false,
+		adaptiveHeight: true
+	});
 });
