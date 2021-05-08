ec2724853a6fc1e9552c6ba0ab0b408ed79ac6c0
aaaaaaaaaaalex <aleks.ru.mail.77@mail.ru>
2021-05-08 18:13:50 +0400

    начал верстать футер

--- yogatur/js/script_jQuery.js
+++ yogatur/js/script_jQuery.js
@@ -3453,4 +3453,21 @@ jQuery(function() {
 		arrows: false,
 		adaptiveHeight: true
 	});
+	jQuery("#slider-3").slick({
+		slidesToShow: 1,
+		arrows: false,
+		adaptiveHeight: true,
+		dots: true
+	});
+	jQuery("#slider-4").slick({
+		slidesToShow: 1,
+		arrows: false,
+		adaptiveHeight: true,
+		dots: true
+	});
+	jQuery("#slider-5").slick({
+		slidesToShow: 1,
+		arrows: true,
+		dots: true
+	});
 });
